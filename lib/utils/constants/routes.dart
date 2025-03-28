8619a500a47a2f91256f8824b2dc4bd0786a2864
Yusufpek <yusuf55ipek55@gmail.com>
2025-03-28 18:50:33 +0300

    constants added

---
+++ lib/utils/constants/routes.dart
@@ -0,0 +1,11 @@
+class Routes {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String home = '/home';
+  static const String userProfile = '/profile';
+  static const String passForget = '/pass_forget';
+  // Bottom Bar Indexes
+  static const int homePageIndex = 1;
+  static const int userProfilePage = 2;
+}
