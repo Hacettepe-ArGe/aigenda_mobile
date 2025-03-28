479f68787a480ff42d2b7db67fa18c141aa59bcc
Yusufpek <yusuf55ipek55@gmail.com>
2025-03-28 20:59:52 +0300

    move files their directories

---
+++ lib/screens/category_list_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import '../models/category_model.dart';
+import '../widgets/category_tile.dart';
+import 'create_category_screen.dart';
+
+class CategoryListScreen extends StatefulWidget {
+  const CategoryListScreen({super.key});
+
+  @override
+  State<CategoryListScreen> createState() => _CategoryListScreenState();
+}
+
+class _CategoryListScreenState extends State<CategoryListScreen> {
+  final List<Category> _categories = [];
+
+  void _openCreateCategory() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => CreateCategoryScreen(
+          onCreate: (newCategory) {
+            setState(() {
+              _categories.add(newCategory);
+            });
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text('Choose Category', style: TextStyle(color: Colors.white)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: _categories.isEmpty
+            ? const Center(
+                child: Text(
+                  'No categories yet.\nTap below to create one!',
+                  style: TextStyle(color: Colors.white70, fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+              )
+            : GridView.builder(
+                itemCount: _categories.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  childAspectRatio: 1,
+                ),
+                itemBuilder: (_, index) {
+                  return CategoryTile(category: _categories[index]);
+                },
+              ),
+      ),
+    );
+  }
+}
