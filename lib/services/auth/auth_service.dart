2d84d0dad769407543bc767415119e3ec93feb4b
Yusufpek <yusuf55ipek55@gmail.com>
2025-03-28 18:51:03 +0300

    auth service and user provider added

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,70 @@
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:flutter/material.dart';
+
+import '../../models/user.dart';
+import '../firebase/model_based/user_service.dart';
+
+class AuthService {
+  final auth.FirebaseAuth _firebaseAuth = auth.FirebaseAuth.instance;
+  final UserService _userService = UserService();
+
+  auth.User? getCurrentUser() => _firebaseAuth.currentUser;
+  Future<void> signOut() async => await _firebaseAuth.signOut();
+
+  // Function to merge data from Firebase Auth credentials and Firestore document
+  User mergeUserData(auth.User firebaseUser, User firestoreUser) {
+    return User(
+      id: firebaseUser.uid,
+      email: firebaseUser.email ?? firestoreUser.email,
+      createdAt: firestoreUser.createdAt,
+    );
+  }
+
+  Future<User?> signInWithEmailAndPassword(String email, String password) async {
+    try {
+      debugPrint('Attempting to sign in with email: $email');
+
+      auth.UserCredential userCredential = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      auth.User firebaseUser = userCredential.user!;
+
+      // Fix User DocumentSnapshot cast error
+      debugPrint('Successfully signed in. UID: ${firebaseUser.uid}');
+      User? firebaseUserObject = await _userService.getDocument(firebaseUser.uid);
+
+      if (firebaseUserObject != null) {
+        debugPrint('User document found in Firestore. Merging data.');
+
+        User mergedUser = mergeUserData(firebaseUser, firebaseUserObject);
+        debugPrint('User data merged successfully.');
+
+        return mergedUser;
+      } else {
+        debugPrint('User document does not exist in Firestore.');
+        return null;
+      }
+    } on auth.FirebaseAuthException catch (e) {
+      debugPrint('FirebaseAuthException: ${e.message}');
+      return null;
+    } catch (e) {
+      debugPrint('Unexpected error: $e');
+      return null;
+    }
+  }
+
+  Future<bool> registerUser(String mail, String password) async {
+    try {
+      debugPrint('Attempting to register with email: $mail');
+      final response = await _firebaseAuth.createUserWithEmailAndPassword(email: mail, password: password);
+      if (response.user != null) {
+        return true;
+      }
+    } catch (e) {
+      debugPrint('Unexpected error: $e.');
+    }
+    return false;
+  }
+}
---
+++ lib/services/providers/user_provider.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../../models/user.dart';
+import '../auth/auth_service.dart';
+import '../firebase/model_based/user_service.dart';
+
+class UserProvider with ChangeNotifier {
+  final AuthService _authService = AuthService();
+  User? user;
+  bool isAuthenticated = false;
+
+  void setUser(User user) {
+    this.user = user;
+    isAuthenticated = true;
+    notifyListeners();
+  }
+
+  Future<void> refreshUser() async {
+    try {
+      final authUser = _authService.getCurrentUser();
+      if (authUser == null) {
+        isAuthenticated = false;
+        user = null;
+        notifyListeners();
+        return;
+      }
+
+      final firestoreUser = await UserService().getDocument(authUser.uid);
+      if (firestoreUser != null) {
+        user = _authService.mergeUserData(authUser, firestoreUser);
+        isAuthenticated = true;
+        notifyListeners();
+        return;
+      } else {
+        user = null;
+        notifyListeners();
+        return;
+      }
+    } catch (e) {
+      isAuthenticated = false;
+      user = null;
+      notifyListeners();
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await _authService.signOut();
+      user = null;
+      isAuthenticated = false;
+      notifyListeners();
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+}
