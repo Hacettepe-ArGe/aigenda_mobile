c00c60606ee566038b7f8dc04897e4db25b787c3
Yusufpek <yusuf55ipek55@gmail.com>
2025-03-28 18:51:35 +0300

    routes, provider and firebase initialize added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:aigenda_mobile/screens/home_screen.dart';
+import 'package:aigenda_mobile/screens/login_screen.dart';
+import 'package:aigenda_mobile/screens/register_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'services/firebase/firebase_service.dart';
 import 'screens/splash_screen.dart';
+import 'services/providers/user_provider.dart';
+import 'utils/constants/routes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FirebaseService.initialize();
   runApp(const MainApp());
 }
 
@@ -10,9 +19,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<UserProvider>(create: (_) => UserProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: Routes.splash,
+        routes: {
+          Routes.splash: (context) => const SplashScreen(),
+          Routes.login: (context) => const LoginScreen(),
+          Routes.register: (context) => const RegisterScreen(),
+          Routes.home: (context) => const HomeScreen(),
+        },
+      ),
     );
   }
 }
